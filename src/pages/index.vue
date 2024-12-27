8eba64d5ae06980582917a382cb0c254a6f8f8aa
w4ng3 <w2gdong@gmail.com>
2024-12-27 16:55:13 +0800

    preset init

---
+++ src/pages/index.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: weisheng
+ * @Date: 2024-11-01 11:44:38
+ * @LastEditTime: 2024-11-01 14:10:20
+ * @LastEditors: weisheng
+ * @Description:
+ * @FilePath: \wot-demo\src\pages\index.vue
+ * 记得注释
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <view class="px-3 py-20 text-center">
+    <view class="mb-4 text-xl text-gray-9">
+      hi！你好。
+    </view>
+  </view>
+</template>
+
+<style></style>
+
+<route type="home" lang="json">
+{
+  "layout": "tabbar",
+  "name": "home",
+  "style": {
+    "navigationBarTitleText": "home"
+  }
+}
+</route>
